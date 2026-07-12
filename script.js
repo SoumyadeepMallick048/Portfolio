ae47a88a2638465485f8d73d0501a10539c16ace
SoumyadeepMallick048 <soumyadeepmallick987@gmail.com>
2026-07-12 20:13:53 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(sec => {
+
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 250;
+
+        if (top > offset) {
+
+            sec.classList.add("active");
+
+        }
+
+    });
+
+});
+
+sections.forEach(sec => {
+
+    sec.classList.add("reveal");
+
+});
